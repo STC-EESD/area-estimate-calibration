40273970e94de9cfcd46d6e257efd12b867de708
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2023-06-12 11:50:48 -0400

    added: 801-Hidiroglou/

---
+++ 801-Hidiroglou/code/main.R
@@ -0,0 +1,59 @@
+
+command.arguments <- commandArgs(trailingOnly = TRUE);
+data.directory    <- normalizePath(command.arguments[1]);
+code.directory    <- normalizePath(command.arguments[2]);
+output.directory  <- normalizePath(command.arguments[3]);
+
+cat("\ndata.directory:",   data.directory,   "\n");
+cat("\ncode.directory:",   code.directory,   "\n");
+cat("\noutput.directory:", output.directory, "\n");
+
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+start.proc.time <- proc.time();
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+# set working directory to output directory
+setwd( output.directory );
+
+##################################################
+# source supporting R code
+code.files <- c(
+    # 'compute-metrics.R',
+    # 'SpatRaster-to-polygons.R',
+    # 'collapse-classes.R',
+    # 'get-nearest-grid-point.R',
+    # 'get-sub-spatraster.R',
+    # 'test-SpatRaster-to-polygons.R'
+    );
+
+for ( code.file in code.files ) {
+    source(file.path(code.directory,code.file));
+    }
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+my.seed <- 7654321;
+set.seed(my.seed);
+
+is.macOS <- grepl(x = sessionInfo()[['platform']], pattern = 'apple', ignore.case = TRUE);
+n.cores  <- ifelse(test = is.macOS, yes = 2, no = parallel::detectCores() - 1);
+cat(paste0("\n# n.cores = ",n.cores,"\n"));
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
+##################################################
+print( warnings() );
+
+print( getOption('repos') );
+
+print( .libPaths() );
+
+print( sessionInfo() );
+
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+stop.proc.time <- proc.time();
+print( stop.proc.time - start.proc.time );
