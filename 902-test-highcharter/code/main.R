1f473cc120f2135c8fb563c2b67bf43f0eeb27da
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2023-08-26 21:29:11 -0400

    added: 902-test-highcharter/

---
+++ 902-test-highcharter/code/main.R
@@ -0,0 +1,67 @@
+
+command.arguments   <- commandArgs(trailingOnly = TRUE);
+data.directory      <- normalizePath(command.arguments[1]);
+code.directory      <- normalizePath(command.arguments[2]);
+output.directory    <- normalizePath(command.arguments[3]);
+google.drive.folder <- command.arguments[4];
+resolution          <- command.arguments[5];
+
+cat("\ndata.directory:",      data.directory,      "\n");
+cat("\ncode.directory:",      code.directory,      "\n");
+cat("\noutput.directory:",    output.directory,    "\n");
+cat("\ngoogle.drive.folder:", google.drive.folder, "\n");
+cat("\nresolution:",          resolution,          "\n");
+
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+start.proc.time <- proc.time();
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+# set working directory to output directory
+setwd( output.directory );
+
+##################################################
+require(jsonlite);
+require(sf);
+require(sfarrow);
+require(stringr);
+require(terra);
+
+# source supporting R code
+code.files <- c();
+for ( code.file in code.files ) {
+    source(file.path(code.directory,code.file));
+    }
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+my.seed <- 7654321;
+set.seed(my.seed);
+
+is.macOS <- grepl(x = sessionInfo()[['platform']], pattern = 'apple', ignore.case = TRUE);
+n.cores  <- ifelse(test = is.macOS, yes = 2, no = parallel::detectCores() - 1);
+cat(paste0("\n# n.cores = ",n.cores,"\n"));
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+dashboard.file <- "dashboard-highcharter";
+rmarkdown::render(
+    input         = file.path(code.directory,paste0(dashboard.file,".Rmd")),
+    output_format = flexdashboard::flex_dashboard(theme = "cerulean"), # darkly
+    output_file   = file.path(output.directory,paste0(dashboard.file,".html"))
+    );
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
+##################################################
+print( warnings() );
+
+print( getOption('repos') );
+
+print( .libPaths() );
+
+print( sessionInfo() );
+
+print( format(Sys.time(),"%Y-%m-%d %T %Z") );
+
+stop.proc.time <- proc.time();
+print( stop.proc.time - start.proc.time );
